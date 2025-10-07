5c89d75ee245452328ffeb557ea20e2ff92b7e0d
Unknows <tonmon2549.mon@gmail.com>
2025-10-07 22:04:59 +0700

    Constructs a new Node

--- src/main/java/hello1/koddata/net/Node.java
+++ src/main/java/hello1/koddata/net/Node.java
@@ -8,4 +8,17 @@ public class Node {
     private SocketChannel channel;
     private ByteBuffer readBuffer;
 
+    public Node(SocketChannel channel , ByteBuffer readBuffer){
+        this.channel = channel;
+        this.readBuffer = readBuffer;
+    }
+
+    public SocketChannel getChannel() {
+        return channel;
+    }
+
+    public ByteBuffer getReadBuffer() {
+        return readBuffer;
+    }
+
 }
