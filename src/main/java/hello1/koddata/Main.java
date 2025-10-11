b928112c4ec054d73b75f0fdb930705955cf3c37
naphon <thewhalezaza900@gmail.com>
2025-10-11 21:51:50 +0700

    Remove unused import in main

--- src/main/java/hello1/koddata/Main.java
+++ src/main/java/hello1/koddata/Main.java
@@ -1,12 +1,8 @@
 package hello1.koddata;
 
-import hello1.koddata.memory.Memory;
 import hello1.koddata.memory.WritableMemory;
-import hello1.koddata.net.SocketFactory;
 
 import java.io.IOException;
-import java.nio.channels.SocketChannel;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,5 +12,6 @@ public class Main {
         long l = memory.readLong();
         System.out.println(l);
         memory.free();
+
     }
 }
