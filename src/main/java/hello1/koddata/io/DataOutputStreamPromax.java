730166b37ad9bc7afc15835a32dca282f7e5f295
Neo <phonlawat@gmail.com>
2025-10-17 23:14:16 +0700

    add DataOutputStreamPromax

--- src/main/java/hello1/koddata/io/DataOutputStreamPromax.java
+++ src/main/java/hello1/koddata/io/DataOutputStreamPromax.java
@@ -1,6 +1,24 @@
 package hello1.koddata.io;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Objects;
 
 public abstract class DataOutputStreamPromax extends OutputStream implements DataOutputPromax {
+    @Override
+    public void write(byte[] b) throws IOException{
+        Objects.requireNonNull(b,"array must not be null (write(byte[]))");
+        write(b,0,b.length);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        Objects.requireNonNull(b, "array must not be null (write(byte[], off, len))");
+        if (off < 0 || len < 0 || off + len > b.length) {
+            throw new IndexOutOfBoundsException("Offset and length out of bounds.");
+        }
+        for (int i = 0; i < len; i++) {
+            write(b[off + i]);
+        }
+    }
 }
