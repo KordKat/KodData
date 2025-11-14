ae38e16208c30524dd8884fba570721c14838bb1
Neo <phonlawat@gmail.com>
2025-11-15 00:17:44 +0700

    make Operation(AbsoluteOperation)

---
+++ src/main/java/hello1/koddata/engine/AbsoluteOperation.java
@@ -0,0 +1,19 @@
+package hello1.koddata.engine;
+
+public class AbsoluteOperation implements QueryOperation {
+    @Override
+    public Value<?> operate(Value<?> value) {
+        if (value == null || value.get() == null) {
+            return null;
+        }
+//        Number number = (Number) value.get();
+
+        if(!(value.get() instanceof Number number)){
+            return null;
+        }
+
+        double result = Math.abs(number.doubleValue());
+
+        return new Value<>(result);
+    }
+}
