1f739ec6bd63b10644a73e5efba6482bb65498e8
naphon <thewhalezaza900@gmail.com>
2025-11-17 22:16:11 +0700

    SessionSettings

--- src/main/java/hello1/koddata/sessions/SessionSettings.java
+++ src/main/java/hello1/koddata/sessions/SessionSettings.java
@@ -1,10 +1,5 @@
 package hello1.koddata.sessions;
 
-public class SessionSettings {
+public record SessionSettings(int maxProcessInSession, int maxMemoryInSession) {
 
-    private int maxVisitorInSession = -1; // this session can have more than 1 visitor at a time
-    private int maxSessionProcess = -1; // this session can have unlimited process
-    private boolean interruptableSession = false; // this session cannot be interrupted by higher priority session except admin
-    private boolean canInterruptAnotherSession = false; // this session cannot interrupt lower priority session
-    private long maximumAllocatedMemoryByte = -1;
 }
